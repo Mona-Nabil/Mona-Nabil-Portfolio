950fa641a4a1664970a190fe5d612dbcd5e6c2de
Mona-Nabil <mbellanbil@gmail.com>
2024-03-04 05:38:40 +0200

    updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./about.css";
-// import aboutImage from "../../assets/about.jpg";
 import redriding from "../../assets/redriding.jpg";
 
 import { FaAward } from "react-icons/fa";

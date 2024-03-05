af91dfd7a4f8f17e83dd927242fd222d5c311152
Mona-Nabil <mbellanbil@gmail.com>
2024-03-05 07:17:31 +0200

    media queries updates

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { FiDribbble } from "react-icons/fi";
+import { FaGlobeAmericas } from "react-icons/fa";
+//  eslint-disable-next-line
+
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/mona-nabil" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/mona-nabil"
+        target="_blank"
+        rel="noreferrer"
+      >
         <BsLinkedin />
       </a>
-      <a href="https://github.com/Mona-Nabil" target="_blank">
+      <a href="https://github.com/Mona-Nabil" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
-      <a href="https://monanabil.com" target="_blank">
-        <FiDribbble />
+      <a href="https://monanabil.com" target="_blank" rel="noreferrer">
+        <FaGlobeAmericas />
       </a>
     </div>
   );

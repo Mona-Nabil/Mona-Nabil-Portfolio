6995b63b1545a1a5aca388d60d7759946584dc46
Mona-Nabil <mbellanbil@gmail.com>
2024-03-08 17:49:29 +0200

    img

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
         <div className="me">
-          <img src={mesky} alt="me" />
+          <img className="me-img" src={mesky} alt="me" />
         </div>
 
         <a href="#contact" className="scroll__down">
